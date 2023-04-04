0b8cd37fc782e468484bece6a236b882df640536
Masum <masumt999.com>
2023-04-05 02:43:54 +0600

    footer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
       <footer className='px-4 divide-y  text-gray-800 relative bottom-0 left-0'>
         <div className='text-sm text-center text-blue-400'>
-          © 2023 BookFair. All rights reserved.
+          © 2023 MRM@BookFair. All rights reserved.
         </div>
       </footer>
     )
